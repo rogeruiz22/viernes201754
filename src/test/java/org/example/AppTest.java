dd6e296afdb62a7833f6d96836e5c85415cdd292
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-20 17:30:07 -0400

    First commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void test() {
+        assertTrue( true );
+    }
+}
